180c93a4e338de684ded583594cbdd0f4b8b0f20
bulletmagnet <bulletmagnet@bulletdesktop>
2016-09-11 18:30:01 -0700

    main method to do crap

---
+++ javain24hours/src/javain24hours/Javain24hours.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javain24hours;
+
+/**
+ *
+ * @author bulletmagnet
+ */
+public class Javain24hours {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
